968bc0f9657cfbecd988efc1e6edd430cfc26a56
unknown <macherki.nessrine@yahoo.com>
2024-05-07 16:25:51 +0100

    add courses file

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var button = document.getElementById('hamburger');
 var links = document.getElementById('items');
 
-button.addEventListener('click', () => {
-    links.style.display = 'block';
-    
-
-
+button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    if (links.style.display === 'block') {
+        links.style.display = 'none';
+    } else {
+        links.style.display = 'block';
+    }
 });
 
 document.body.addEventListener('click', function(event) {
-    if (!links.contains(event.target) && !button.contains(event.target)) {
-        links.style.display = 'none';    
+    if (window.innerWidth <= 768 && !links.contains(event.target)){ if (!links.contains(event.target) && event.target !== button) {
+        links.style.display = 'none';
+    }}
+    else {
+        links.style.display = 'block';
     }
 });

0a03e2f5ff3863702b81c64ecd7f91408211f9fa
unknown <macherki.nessrine@yahoo.com>
2024-05-07 09:43:14 +0100

    add expanded menulist

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function myFunction() {
-    // Get the element with the id "navbar"
-    var x = document.getElementById("navbar");
+var button = document.getElementById('hamburger');
+var links = document.getElementById('items');
+
+button.addEventListener('click', () => {
+    links.style.display = 'block';
     
-    // Check if the class name of the element is "navbar"
-    if (x.className === "navbar") {
-        // If it is, add the class "responsive" to enable responsive behavior
-        x.className += " responsive";
-    } else {
-        // If it's not, remove the class "responsive" to disable responsive behavior
-        x.className = "navbar";
+
+
+});
+
+document.body.addEventListener('click', function(event) {
+    if (!links.contains(event.target) && !button.contains(event.target)) {
+        links.style.display = 'none';    
     }
-}
+});
